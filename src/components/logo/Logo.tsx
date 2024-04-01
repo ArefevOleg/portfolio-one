75c20a78b58c984b33e31f15125d958fbac8c427
Arefevpro <arefevmsk@gmail.com>
2024-04-01 19:04:06 +0300

    ./

---
+++ src/components/logo/Logo.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Icon } from '../../components/icon/Icon';
+
+export const Logo = () => {
+  return (
+    <a href="">
+    <Icon  iconId={'codeSvg'}/>
+  </a>
+
+  );
+};
